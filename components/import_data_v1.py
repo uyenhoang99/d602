b75b33576d7be9f036960b231e588441769aa78c
uyenhoang99 <uhoang67@gmail.com>
2026-02-03 21:54:02 -0800

    update file

---
+++ components/import_data_v1.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+"""
+Data Import Script - Version 1
+D602 Task 2 - Flight Delay Analysis
+
+This script imports data from the downloaded csv file.
+
+"""
+
+import pandas as pd
+import numpy as np
+
+def import_data():
+    """
+    Import data from the downloaded raw CSV file extracted from the Bureau of Transportation Statistics.
+    This is the initial version with basic functionality.
+    """
+    try: 
+        # Import the dataset
+        print("Importing the dataset")
+        df = 
+    
+    except:
+        pass
+
+
