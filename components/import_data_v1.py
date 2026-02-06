d1aa1baeef90612af4d5ef7b8d69d3c686b5912d
uyenhoang99 <uhoang67@gmail.com>
2026-02-06 11:20:45 -0800

    create and update files

--- components/import_data_v1.py
+++ components/import_data_v1.py
@@ -9,7 +9,6 @@ This script imports data from the downloaded csv file.
 """
 
 import pandas as pd
-import numpy as np
 
 def import_data():
     """
@@ -19,9 +18,34 @@ def import_data():
     try: 
         # Import the dataset
         print("Importing the dataset")
-        df = 
+        df = pd.read_csv("C:/Users/uyen/Desktop/d602/notebook/T_ONTIME_REPORTING.csv")
+
+        # Inspect the dataset
+        print("\nCounting the number of columns and rows of the dataset:")
+        col_count = df.shape[0]
+        row_count = df.shape[1]
+        print("There are ", col_count, "columns and ", row_count, "rows in this dataset")
+
+        # Inspect the column names
+        print("\nObserve the column names")
+        print("Column names: ", df.columns)
+
+        # Output the raw imported data
+        print("\nOuput the raw imported dataset into the artifacts folder")
+        df.to_csv("artifacts/data.csv", index = False, header = True)
+
+        # Complete the data import process
+        print("\nData has been successfully imported")
+
+
     
-    except:
-        pass
+    except FileNotFoundError:
+        print("Error: file not found. Check the file address.")
+        return None
+        
 
 
+
+
+if __name__ == "__main__":
+    import_data()
